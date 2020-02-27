e097ad77cf77177229702bb89d6e50a630d08518
Fred Spieler <fspieler@gmail.com>
2020-02-27 00:28:42 -0500

    fizzbuzz func configurable with kwargs

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,22 @@
-def fizzbuzz(ul=20):
-    items = [
-        (3, 'fizz'),
-        (5, 'buzz')
-    ]
-
+def fizzbuzz(ul=20, **kwargs):
+    if not kwargs:
+        kwargs = {
+            'fizz': 3,
+            'buzz': 5
+        }
+    items = sorted(kwargs.items(), key=lambda t:t[1])
     def fn(i, s, idx):
         if idx == -1:
             return s if s else i
         return fn(
             i,
-            f'{items[idx][1]}{s}' if i % items[idx][0] == 0 else s,
+            f'{items[idx][0]}{s}' if i % items[idx][1] == 0 else s,
             idx-1
         )
 
-    return (fn(i, '', len(items)-1) for i in range(1,ul))
+    return (fn(i, '', len(items)-1) for i in range(1,ul+1))
 
-for val in fizzbuzz(20):
+for val in fizzbuzz(41, fizz=3, buzz=4):
     print(val)
 
 
